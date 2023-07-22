0dc43cee5de19ce348a99a0c0b09cffb99bf303a
Ali Beyaz <symbuzzer@users.noreply.github.com>
2023-07-22 19:21:06 +0300

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,6 +1,2 @@
 mkdir -p "$MODPATH/system/etc"
-cp -af /system/etc/hosts "$MODPATH/system/etc/hosts"
-chcon -r u:object_r:system_file:s0 "$MODPATH/system"
-chmod 644 "$MODPATH/system/etc/hosts"
-mkdir "$MODPATH/worker"
-touch "$MODPATH/skip_mount"
+cp -af /system/etc/hosts "$MODPATH/system/etc"
