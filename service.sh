6aeb2212084c9010f8a661bd5a3b81ebabd279b3
Ali Beyaz <symbuzzer@users.noreply.github.com>
2023-07-22 19:21:29 +0300

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,3 +1,2 @@
-# name this mountpoint as KSU to trigger unmount
 MODDIR="${0%/*}"
-mount -t overlay -o lowerdir=/system/etc,upperdir=$MODDIR/system/etc,workdir=$MODDIR/worker KSU /system/etc
+mount --bind "$MODDIR/system/etc/hosts" /system/etc/hosts
